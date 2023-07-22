3a163504c4c12738adb713e566e40e6519b2b033
Externet <timocop@hotmail.de>
2023-07-22 23:37:10 +0200

    Update driver version

--- PSMSVirtualDeviceManager/Classes/ClassVmtConst.vb
+++ PSMSVirtualDeviceManager/Classes/ClassVmtConst.vb
@@ -1,6 +1,6 @@
 ﻿Public Class ClassVmtConst
     Public Const VMT_TRACKER_MAX As Integer = 16 'Max trackers we can have, reserve 10 for tracking references
-    Public Const VMT_DRIVER_VERSION_EXPECT As String = "VMT_013"
+    Public Const VMT_DRIVER_VERSION_EXPECT As String = "VMTX_014"
 
     Public Const VMT_IP As String = "127.0.0.1"
     Public Const VMT_PORT_RECEIVE As Integer = 34571
