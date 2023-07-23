6d1e79e4b4b6a946ca108315daf3fbe3b0de73ae
Externet <timocop@hotmail.de>
2023-07-23 21:19:51 +0200

    Switch tab when Startin/Pausing OSC

--- PSMSVirtualDeviceManager/FormMain.vb
+++ PSMSVirtualDeviceManager/FormMain.vb
@@ -219,6 +219,7 @@ Public Class FormMain
 
         SelectPage(ENUM_PAGE.VIRTUAL_CONTROLLERS)
         g_mUCVirtualControllers.TabControl1.SelectedTab = g_mUCVirtualControllers.TabPage_VMT
+        g_mUCVirtualControllers.g_mUCVirtualMotionTracker.TabControl1.SelectedTab = g_mUCVirtualControllers.g_mUCVirtualMotionTracker.TabPage_Management
 
         g_mUCVirtualControllers.g_mUCVirtualMotionTracker.LinkLabel_OscRun_Click()
     End Sub
@@ -230,6 +231,7 @@ Public Class FormMain
 
         SelectPage(ENUM_PAGE.VIRTUAL_CONTROLLERS)
         g_mUCVirtualControllers.TabControl1.SelectedTab = g_mUCVirtualControllers.TabPage_VMT
+        g_mUCVirtualControllers.g_mUCVirtualMotionTracker.TabControl1.SelectedTab = g_mUCVirtualControllers.g_mUCVirtualMotionTracker.TabPage_Management
 
         g_mUCVirtualControllers.g_mUCVirtualMotionTracker.LinkLabel_OscPause_Click()
     End Sub
