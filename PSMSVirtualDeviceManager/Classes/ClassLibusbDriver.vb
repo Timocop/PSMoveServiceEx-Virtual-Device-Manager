8e85b47f7050d96621fe2ef30f05d33722d536b1
Externet <timocop@hotmail.de>
2023-09-17 18:31:49 +0200

    Check for already existing libusb drivers

--- PSMSVirtualDeviceManager/Classes/ClassLibusbDriver.vb
+++ PSMSVirtualDeviceManager/Classes/ClassLibusbDriver.vb
@@ -1,4 +1,9 @@
-﻿Public Class ClassLibusbDriver
+﻿Imports Microsoft.Win32
+
+Public Class ClassLibusbDriver
+    Public Const PROVIDER_NAME As String = "libusb-win32"
+    Public Const PROVIDER_VERSION As String = "1.2.6.0"
+
     Public Const DRV_ROOT_NAME As String = "libusb_driver"
     Public Const DRV_INSTALLER_NAME As String = "wdi-simple.exe"
     'Public ReadOnly DRV_PSEYE_INSTALLER_CONFIGS As STRUC_DEVICE_DRIVER_INFO() = {
@@ -118,4 +123,148 @@
         End Using
     End Sub
 
+    <Flags>
+    Public Enum DEVICE_CONFIG_FLAGS As Integer
+        ' Device is disabled
+        CONFIGFLAG_DISABLED = &H1
+
+        ' Device has been removed or uninstalled
+        CONFIGFLAG_REMOVED = &H2
+
+        ' Device was manually installed
+        CONFIGFLAG_MANUAL_INSTALL = &H4
+
+        ' Ignore boot configuration for the device
+        CONFIGFLAG_IGNORE_BOOT_LC = &H8
+
+        ' Device is used for network booting
+        CONFIGFLAG_NET_BOOT = &H10
+
+        ' Device should be reinstalled
+        CONFIGFLAG_REINSTALL = &H20
+
+        ' Device installation has failed
+        CONFIGFLAG_FAILEDINSTALL = &H40
+
+        ' Device can't be stopped because it has child devices
+        CONFIGFLAG_CANTSTOPACHILD = &H80
+
+        ' It is safe to remove the device's ROM
+        CONFIGFLAG_OKREMOVEROM = &H100
+
+        ' No "remove" action should be taken when the device is removed
+        CONFIGFLAG_NOREMOVEEXIT = &H200
+
+        ' Device is disabled in the current hardware profile
+        CONFIGFLAG_HWPROFILE_DISABLED = &H800000
+
+        ' Device is enabled in the current hardware profile
+        CONFIGFLAG_HWPROFILE_ENABLED = &H400000
+
+        ' The system has attempted to restart the device
+        CONFIGFLAG_RESTART_REQUIRED = &H200000
+    End Enum
+
+    Structure STRUC_DEVICE_PROVIDER
+        Dim sDeviceID As String
+        Dim iConfigFlags As DEVICE_CONFIG_FLAGS
+        Dim sProviderName As String
+        Dim sProviderVersion As String
+
+        Public Function IsEnabled() As Boolean
+            Return ((iConfigFlags And DEVICE_CONFIG_FLAGS.CONFIGFLAG_DISABLED) = 0)
+        End Function
+
+        Public Function IsRemoved() As Boolean
+            Return ((iConfigFlags And DEVICE_CONFIG_FLAGS.CONFIGFLAG_REMOVED) <> 0)
+        End Function
+
+        Public Function HasDriverInstalled() As Boolean
+            Return (sProviderName Is Nothing OrElse sProviderVersion Is Nothing)
+        End Function
+    End Structure
+
+    Public Function IsDeviceLibusb(mInfo As STRUC_DEVICE_DRIVER_INFO) As Boolean
+        Return IsDeviceLibusb(mInfo.VID, mInfo.PID, mInfo.MM)
+    End Function
+
+    Public Function IsDeviceLibusb(sVID As String, sPID As String, sMI As String) As Boolean
+        For Each mInfo In GetDeviceProvider(sVID, sPID, sMI)
+            If (mInfo.sProviderName Is Nothing OrElse mInfo.sProviderVersion Is Nothing) Then
+                Return False
+            End If
+
+            If (mInfo.sProviderName <> PROVIDER_NAME OrElse mInfo.sProviderVersion <> PROVIDER_VERSION) Then
+                Return False
+            End If
+        Next
+
+        Return True
+    End Function
+
+    Public Function GetDeviceProvider(mInfo As STRUC_DEVICE_DRIVER_INFO) As STRUC_DEVICE_PROVIDER()
+        Return GetDeviceProvider(mInfo.VID, mInfo.PID, mInfo.MM)
+    End Function
+
+    Public Function GetDeviceProvider(sVID As String, sPID As String, sMI As String) As STRUC_DEVICE_PROVIDER()
+        Dim mProviderList As New List(Of STRUC_DEVICE_PROVIDER)
+
+        Dim sUseDevice As String = Nothing
+        If (String.IsNullOrEmpty(sMI)) Then
+            sUseDevice = String.Format("USB\VID_{0}&PID_{1}", sVID, sPID)
+        Else
+            If (sMI.Length = 1) Then
+                sMI = "0" & sMI
+            End If
+
+            sUseDevice = String.Format("USB\VID_{0}&PID_{1}&MI_{2}", sVID, sPID, sMI)
+        End If
+
+        Dim mUsbDeviceKey As RegistryKey = Registry.LocalMachine.OpenSubKey(String.Format("SYSTEM\CurrentControlSet\Enum\{0}", sUseDevice))
+        If (mUsbDeviceKey Is Nothing) Then
+            Return mProviderList.ToArray
+        End If
+
+        For Each mDeviceSubKey As String In mUsbDeviceKey.GetSubKeyNames()
+            Dim mDeviceKey As RegistryKey = mUsbDeviceKey.OpenSubKey(mDeviceSubKey)
+            If (mDeviceKey Is Nothing) Then
+                Continue For
+            End If
+
+            Dim sDeviceID As String = String.Format("{0}\{1}", sUseDevice, mDeviceSubKey)
+
+            Dim sDriverLocation As String = TryCast(mDeviceKey.GetValue("Driver"), String)
+            Dim sConfigFlags As DEVICE_CONFIG_FLAGS = CType(mDeviceKey.GetValue("ConfigFlags", 0), DEVICE_CONFIG_FLAGS)
+
+            If (sDriverLocation IsNot Nothing) Then
+                Dim mDriverKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Control\Class\" & sDriverLocation)
+                If (mDriverKey Is Nothing) Then
+                    Continue For
+                End If
+
+                Dim sProviderName As String = TryCast(mDriverKey.GetValue("ProviderName"), String)
+                Dim sProviderVersion As String = TryCast(mDriverKey.GetValue("DriverVersion"), String)
+                If (sProviderVersion IsNot Nothing AndAlso sProviderName IsNot Nothing) Then
+                    Dim mInfo As New STRUC_DEVICE_PROVIDER
+                    mInfo.sDeviceID = sDeviceID
+                    mInfo.iConfigFlags = sConfigFlags
+                    mInfo.sProviderName = sProviderName
+                    mInfo.sProviderVersion = sProviderVersion
+
+                    mProviderList.Add(mInfo)
+                End If
+            Else
+                ' No driver installed
+                Dim mInfo As New STRUC_DEVICE_PROVIDER
+                mInfo.sDeviceID = sDeviceID
+                mInfo.iConfigFlags = sConfigFlags
+                mInfo.sProviderName = Nothing
+                mInfo.sProviderVersion = Nothing
+
+                mProviderList.Add(mInfo)
+            End If
+        Next
+
+        Return mProviderList.ToArray
+    End Function
 End Class
