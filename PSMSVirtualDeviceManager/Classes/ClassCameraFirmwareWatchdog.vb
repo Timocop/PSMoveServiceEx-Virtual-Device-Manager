ed568505911b738d62a2d3162db2f9a110b7fc35
Externet <timocop@hotmail.de>
2024-10-19 14:02:07 +0200

    Use PS4 Camera VID and PID from libusb class instead

--- PSMSVirtualDeviceManager/Classes/ClassCameraFirmwareWatchdog.vb
+++ PSMSVirtualDeviceManager/Classes/ClassCameraFirmwareWatchdog.vb
@@ -3,56 +3,12 @@
 
     Private g_bInit As Boolean = False
 
-    Public ReadOnly FIRM_PS4CAM_VIDEO As STRUC_DEVICE_DRIVER_INFO() = {
-        New STRUC_DEVICE_DRIVER_INFO("USB PlayStation 4 Gen 1 Stereo Camera", "05A9", "0580", "05A9", "058A"),
-        New STRUC_DEVICE_DRIVER_INFO("USB PlayStation 4 Gen 2 Stereo Camera", "05A9", "0580", "05A9", "058B")
-    }
-
     Private g_ClassUsbNotify As ClassDevicesNotify = Nothing
 
     Private g_mHardwareChangeStatusThread As Threading.Thread = Nothing
     Private g_bHardwareChangeStatusUpdatenNow As Boolean = False
 
     Private Shared _ThreadLock As New Object
-
-    Structure STRUC_DEVICE_DRIVER_INFO
-        Dim sName As String
-        Dim VID As String
-        Dim PID As String
-        Dim VID_V As String
-        Dim PID_V As String
-
-        Public Sub New(_Name As String, _VID As String, _PID As String, _VID_V As String, _PID_V As String)
-            sName = _Name
-            VID = _VID
-            PID = _PID
-            VID_V = _VID_V
-            PID_V = _PID_V
-        End Sub
-
-        Public Sub RunFirmwareUploader()
-            Dim sRootFolder As String = IO.Path.Combine(IO.Path.GetDirectoryName(Application.ExecutablePath), ClassLibusbDriver.DRV_PS4CAM_ROOT_NAME)
-            Dim sInstallerPath As String = IO.Path.Combine(sRootFolder, ClassLibusbDriver.DRV_PS4CAM_FIRMWARE_NAME)
-            Dim sFirmwarePath As String = IO.Path.Combine(sRootFolder, ClassLibusbDriver.DRV_PS4CAM_FIRMWARE_BIN_NAME)
-
-            If (Not IO.File.Exists(sFirmwarePath)) Then
-                Throw New ArgumentException("Firmware file does not exist")
-            End If
-
-            Using mProcess As New Process
-                mProcess.StartInfo.FileName = sInstallerPath
-                mProcess.StartInfo.Arguments = ""
-                mProcess.StartInfo.WorkingDirectory = sRootFolder
-                mProcess.StartInfo.CreateNoWindow = True
-                mProcess.StartInfo.UseShellExecute = True
-                mProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-
-                mProcess.Start()
-
-                mProcess.WaitForExit(60000)
-            End Using
-        End Sub
-    End Structure
 
     Public Sub New()
     End Sub
@@ -81,7 +37,7 @@
 
         Dim bFound As Boolean = False
 
-        For Each mDevice In FIRM_PS4CAM_VIDEO
+        For Each mDevice In ClassLibusbDriver.DRV_PS4CAM_WINUSB_CONFIGS
             Dim sHardwareId As String = String.Format("\USB#VID_{0}&PID_{1}", mDevice.VID, mDevice.PID)
 
             If (sDeviceName.ToLowerInvariant.Contains(sHardwareId.ToLowerInvariant)) Then
@@ -130,13 +86,14 @@
 
     Public Sub UploadFirmware()
         SyncLock _ThreadLock
-            For Each mDevice In FIRM_PS4CAM_VIDEO
+            For Each mDevice In ClassLibusbDriver.DRV_PS4CAM_WINUSB_CONFIGS
                 Dim iTries As Integer = 10
 
                 Dim mLibUsbDriver As New ClassLibusbDriver
                 While (mLibUsbDriver.VerifyPlaystation4CamDriver64() AndAlso
                         mLibUsbDriver.IsUsbDeviceConnected(New ClassLibusbDriver.STRUC_DEVICE_DRIVER_INFO(Nothing, Nothing, mDevice.VID, mDevice.PID, Nothing, Nothing)))
-                    mDevice.RunFirmwareUploader()
+
+                    RunFirmwareUploader()
 
                     iTries -= 1
 
@@ -146,6 +103,29 @@
                 End While
             Next
         End SyncLock
+    End Sub
+
+    Public Sub RunFirmwareUploader()
+        Dim sRootFolder As String = IO.Path.Combine(IO.Path.GetDirectoryName(Application.ExecutablePath), ClassLibusbDriver.DRV_PS4CAM_ROOT_NAME)
+        Dim sInstallerPath As String = IO.Path.Combine(sRootFolder, ClassLibusbDriver.DRV_PS4CAM_FIRMWARE_NAME)
+        Dim sFirmwarePath As String = IO.Path.Combine(sRootFolder, ClassLibusbDriver.DRV_PS4CAM_FIRMWARE_BIN_NAME)
+
+        If (Not IO.File.Exists(sFirmwarePath)) Then
+            Throw New ArgumentException("Firmware file does not exist")
+        End If
+
+        Using mProcess As New Process
+            mProcess.StartInfo.FileName = sInstallerPath
+            mProcess.StartInfo.Arguments = ""
+            mProcess.StartInfo.WorkingDirectory = sRootFolder
+            mProcess.StartInfo.CreateNoWindow = True
+            mProcess.StartInfo.UseShellExecute = True
+            mProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+
+            mProcess.Start()
+
+            mProcess.WaitForExit(60000)
+        End Using
     End Sub
 
 #Region "IDisposable Support"
