525a2332108985840cc66a1cf973f1df4d1ca161
Externet <timocop@hotmail.de>
2024-11-12 19:57:44 +0100

    New Version

--- PSMSVirtualDeviceManager/My Project/AssemblyInfo.vb
+++ PSMSVirtualDeviceManager/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("13.11.4.0")>
-<Assembly: AssemblyFileVersion("13.11.4.0")>
+<Assembly: AssemblyVersion("13.11.5.0")>
+<Assembly: AssemblyFileVersion("13.11.5.0")>
