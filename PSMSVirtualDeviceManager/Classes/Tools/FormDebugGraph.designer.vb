ed91b06f852b30ba6d3d4bc762922c18fc4312a3
Externet <timocop@hotmail.de>
2024-11-21 08:24:14 +0100

    Add debug graph

---
+++ PSMSVirtualDeviceManager/Classes/Tools/FormDebugGraph.designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormDebugGraph
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.Chart_DebugGraph = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearGraphToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripComboBox_Scale = New System.Windows.Forms.ToolStripComboBox()
+        CType(Me.Chart_DebugGraph, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Chart_DebugGraph
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart_DebugGraph.ChartAreas.Add(ChartArea1)
+        Me.Chart_DebugGraph.Dock = System.Windows.Forms.DockStyle.Fill
+        Legend1.Name = "Legend1"
+        Me.Chart_DebugGraph.Legends.Add(Legend1)
+        Me.Chart_DebugGraph.Location = New System.Drawing.Point(0, 27)
+        Me.Chart_DebugGraph.Name = "Chart_DebugGraph"
+        Me.Chart_DebugGraph.Size = New System.Drawing.Size(550, 413)
+        Me.Chart_DebugGraph.TabIndex = 2
+        Me.Chart_DebugGraph.Text = "Chart1"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ClearGraphToolStripMenuItem, Me.ToolStripComboBox_Scale})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(550, 27)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CloseToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 23)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'ClearGraphToolStripMenuItem
+        '
+        Me.ClearGraphToolStripMenuItem.Name = "ClearGraphToolStripMenuItem"
+        Me.ClearGraphToolStripMenuItem.Size = New System.Drawing.Size(81, 23)
+        Me.ClearGraphToolStripMenuItem.Text = "Clear Graph"
+        '
+        'ToolStripComboBox_Scale
+        '
+        Me.ToolStripComboBox_Scale.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ToolStripComboBox_Scale.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ToolStripComboBox_Scale.Name = "ToolStripComboBox_Scale"
+        Me.ToolStripComboBox_Scale.Size = New System.Drawing.Size(121, 23)
+        '
+        'FormDebugGraph
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(550, 440)
+        Me.Controls.Add(Me.Chart_DebugGraph)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MinimizeBox = False
+        Me.Name = "FormDebugGraph"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Debug Graph"
+        CType(Me.Chart_DebugGraph, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Chart_DebugGraph As DataVisualization.Charting.Chart
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearGraphToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripComboBox_Scale As ToolStripComboBox
+End Class
---
+++ PSMSVirtualDeviceManager/Classes/Tools/FormDebugGraph.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Numerics
+
+Public Class FormDebugGraph
+    Private g_bIgnoreEvents As Boolean = False
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Try
+            g_bIgnoreEvents = True
+
+            ToolStripComboBox_Scale.Items.Clear()
+            ToolStripComboBox_Scale.Items.Add("1")
+            ToolStripComboBox_Scale.Items.Add("10")
+            ToolStripComboBox_Scale.Items.Add("100")
+            ToolStripComboBox_Scale.Items.Add("1000")
+            ToolStripComboBox_Scale.Items.Add("10000")
+            ToolStripComboBox_Scale.SelectedIndex = 0
+        Finally
+            g_bIgnoreEvents = False
+        End Try
+    End Sub
+
+    Public Property m_Index As Integer = 0
+    Public Property m_MaxIndex As Integer = 1000
+    Public Property m_Scale As Integer = 1
+
+    Public Sub AddChartValues(sSeriesName As String, iValue As Quaternion)
+        AddChartValues(sSeriesName & ".x", iValue.X)
+        AddChartValues(sSeriesName & ".y", iValue.Y)
+        AddChartValues(sSeriesName & ".z", iValue.Z)
+        AddChartValues(sSeriesName & ".w", iValue.W)
+    End Sub
+
+    Public Sub AddChartValues(sSeriesName As String, iValue As Vector3)
+        AddChartValues(sSeriesName & ".x", iValue.X)
+        AddChartValues(sSeriesName & ".y", iValue.Y)
+        AddChartValues(sSeriesName & ".z", iValue.Z)
+    End Sub
+
+    Public Sub AddChartValues(sSeriesName As String, iValue As Double)
+        If (Not Chart_DebugGraph.Enabled) Then
+            Return
+        End If
+
+        If (Chart_DebugGraph.ChartAreas.Count < 1) Then
+            Return
+        End If
+
+        If (m_Scale < 1) Then
+            m_Scale = 1
+        End If
+
+        iValue *= m_Scale
+
+        If (Chart_DebugGraph.Series.IndexOf(sSeriesName) = -1) Then
+            Dim mSeries = Chart_DebugGraph.Series.Add(sSeriesName)
+
+            mSeries.ChartArea = Chart_DebugGraph.ChartAreas(0).Name
+            mSeries.ChartType = DataVisualization.Charting.SeriesChartType.FastLine
+            mSeries.BorderWidth = 2
+        End If
+
+        Chart_DebugGraph.Series(sSeriesName).Points.AddXY(m_Index, iValue)
+
+        While (Chart_DebugGraph.Series(sSeriesName).Points.Count > 1 AndAlso
+            Chart_DebugGraph.Series(sSeriesName).Points(1).XValue < (m_Index - m_MaxIndex))
+            Chart_DebugGraph.Series(sSeriesName).Points.RemoveAt(0)
+        End While
+
+        ' Adjust bounds
+        Dim mAxisX = Chart_DebugGraph.ChartAreas(0).AxisX()
+        If (mAxisX.Maximum < m_Index) Then
+            mAxisX.Maximum = m_Index
+        End If
+        If (mAxisX.Minimum < (m_Index - m_MaxIndex)) Then
+            mAxisX.Minimum = m_Index - m_MaxIndex
+        End If
+
+        Dim mAxisY = Chart_DebugGraph.ChartAreas(0).AxisY()
+        Dim iValueMax = Math.Ceiling(iValue * 0.1) * 10
+        Dim iValueMin = Math.Floor(iValue * 0.1) * 10
+
+        If (mAxisY.Maximum < iValueMax) Then
+            mAxisY.Maximum = iValueMax
+        End If
+        If (mAxisY.Minimum > iValueMin) Then
+            mAxisY.Minimum = iValueMin
+        End If
+    End Sub
+
+    Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearGraphToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearGraphToolStripMenuItem.Click
+        Chart_DebugGraph.Series.Clear()
+        Chart_DebugGraph.ChartAreas(0).AxisX.Maximum = 1
+        Chart_DebugGraph.ChartAreas(0).AxisX.Minimum = 0
+        Chart_DebugGraph.ChartAreas(0).AxisY.Maximum = 1
+        Chart_DebugGraph.ChartAreas(0).AxisY.Minimum = 0
+    End Sub
+
+    Private Sub ToolStripComboBox_Scale_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ToolStripComboBox_Scale.SelectedIndexChanged
+        If (g_bIgnoreEvents) Then
+            Return
+        End If
+
+        If (ToolStripComboBox_Scale.SelectedItem Is Nothing) Then
+            Return
+        End If
+
+        m_Scale = CInt(ToolStripComboBox_Scale.SelectedItem)
+    End Sub
+End Class
