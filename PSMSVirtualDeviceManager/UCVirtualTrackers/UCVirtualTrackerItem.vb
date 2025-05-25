6ff633868f992042a4aec2f519c8305c88906773
Externet <timocop@hotmail.de>
2025-05-25 15:59:20 +0200

    Add warning

--- PSMSVirtualDeviceManager/UCVirtualTrackers/UCVirtualTrackerItem.vb
+++ PSMSVirtualDeviceManager/UCVirtualTrackers/UCVirtualTrackerItem.vb
@@ -2466,7 +2466,7 @@ Public Class UCVirtualTrackerItem
             Return
         End If
 
-        MessageBox.Show("Manually changing the properties of video input devices is for debugging purposes only and is not saved!", "Warning", MessageBoxButtons.OK)
+        MessageBox.Show("Manually changing the properties of video input devices is for debugging purposes only and is not saved!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
 
         g_mClassCaptureLogic.m_Capture.Settings = 0
         g_mClassCaptureLogic.m_Capture.Settings = 1
